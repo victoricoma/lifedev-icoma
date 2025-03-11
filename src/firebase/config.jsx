aa83ba3c86871fd37fe59e3e72ca2de8888f9516
victoricoma-soulcode <victor.icoma@soulcode.com>
2025-03-11 12:34:39 -0300

    Configuração das Variaveis do Firebase

---
+++ src/firebase/config.jsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgaoc_8HjFS333uhbpjvYz2lfIld62FYk",
+    authDomain: "lifedev-icoma.firebaseapp.com",
+    projectId: "lifedev-icoma",
+    storageBucket: "lifedev-icoma.firebasestorage.app",
+    messagingSenderId: "652379164002",
+    appId: "1:652379164002:web:442fd1fba8292fd873b03b",
+    measurementId: "G-9WPGGKDGRS"
+};
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const db = getFirestore(app)
+
+export { db }
