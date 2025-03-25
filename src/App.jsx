d4d71c69cea53b6229a1c893b24ebae0b3cdd350
victoricoma-soulcode <victor.icoma@soulcode.com>
2025-03-25 12:19:52 -0300

    Aula de Hooks e Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <Home />
+      <Footer />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,22 @@ import { useState } from 'react'
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
-    const [user, setUser] = useState({name:'Victor',idade:42})
+  const [user, setUser] = useState({ name: 'Victor', idade: 42 })
 
   return (
     <>
-    <nav className={styles.navbar}>
-        <p>{user.name}</p>
-        <p>{user.idade}</p>
-        <button onClick={() => setUser((prevUser) => 
-          ({...prevUser, idade: prevUser.idade +1}))}>Aumenta Idade</button>
-    </nav>
+      <nav className={styles.navbar}>
+        <ul className={styles.link_list}>
+          <li>Home</li>
+          <li>Register</li>
+          <li>New Post</li>
+          <li>Dashboard</li>
+          <li>About Us</li>
+          <li>
+            <button className={styles.logout}>Exit</button>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
