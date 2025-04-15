76058b0dd7aefc06fa3bf38fd8babe3995c6c11f
victoricoma <victor.icoma@fatec.sp.gov.br>
2025-04-15 08:29:56 -0300

    Exercício para 2 pontos na Prova ou -1 na prova.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+import './App.css'
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <Home />
-      <Login />
-      <Footer />
+      <div>
+        <BrowserRouter>
+        <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </div>
     </>
   )
 }
