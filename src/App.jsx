e5c23a97127a468a593f661c4acbb1c9394b8e94
victoricoma <victor.icoma@fatec.sp.gov.br>
2025-05-13 11:03:13 -0300

    Parte 1 Aula 1305

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
+
+import { useState, useEffect, use } from 'react'
+
+import { AuthProvider } from './components/context/AuthContext'
+
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 import './App.css'
+import { useAuthentication } from './hooks/useAuthentication'
 
 function App() {
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAuthentication()
 
+  const loadingUser = user == undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, () => {
+      setUser(user)
+    })
+  }, [auth])
+
+  if (loadingUser) {
+    return <p>Carregando a página ...</p>
+  }
   return (
     <>
       <div>
-        <BrowserRouter>
-        <Navbar />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
+        <AuthProvider value={user}>
+          <BrowserRouter>
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+              </Routes>
+            </div>
+            <Footer />
+          </BrowserRouter>
+        </AuthProvider>
       </div>
     </>
   )
